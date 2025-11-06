31b54568b251b151414a9faa675c755610eae5a0
tnxec2 <tnxec2@gmail.com>
2025-11-06 13:57:51 +0100

    fix pagination

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -12,24 +12,24 @@ const showPageNumbers = 2 // number of pagenumbers before and after current page
 
 export const Pagination: FC<Props> = ({currentPage, total, countPerPage, onChangePage}) => {
 
-    const maxPage = useMemo(() => Math.floor(total / countPerPage), [total, countPerPage])
+    const maxPage = useMemo(() => Math.ceil(total / countPerPage), [total, countPerPage])
 
-    const pages = useMemo(() => 
-        { 
-            const result = []
-            const start = Math.max(1, currentPage - showPageNumbers)
-            const end = Math.min(maxPage, currentPage + showPageNumbers)
-            
+    const pages = useMemo(() => { 
+        const result = []
+        const start = Math.max(1, currentPage - showPageNumbers)
+        const end = Math.min(maxPage, currentPage + showPageNumbers)
+        
         for (let p = start; p <= end; p++) {
             result.push(p)
         }
+
         return result
     }, [maxPage, currentPage]
     )
 
     const goTo = () => {
         let p = window.prompt('Go to page');
-        if (Number(p) > 0) onChangePage(Number(p)-1)
+        if (Number(p) > 0) onChangePage(Number(p))
     }
 
     return (
@@ -37,19 +37,19 @@ export const Pagination: FC<Props> = ({currentPage, total, countPerPage, onChang
         { total > 0 ? 
         <div className={styles.pagination}>
             { currentPage > 1 ? <span title="Go to previous page" onClick={() => onChangePage(currentPage-1)} >&laquo;</span> : '' }
-            { currentPage > showPageNumbers ? <span title="Go to first page" onClick={() => onChangePage(1)}>1</span> : '' }
-            { currentPage > showPageNumbers+1 ? <span title="Go to page..." onClick={() => goTo()}>...</span> : '' }
+            { !pages.includes(1) ? <span title="Go to first page" onClick={() => onChangePage(1)}>1</span> : '' }
+            { currentPage > showPageNumbers+2 ? <span title="Go to page..." onClick={() => goTo()}>...</span> : '' }
 
             { pages.map((page) =>
                  <span key={'page-' + page} 
-                    title={`Go to page ${page+1}`}
+                    title={`Go to page ${page}`}
                     className={`${currentPage === page ? styles.active : ''}`}
                     onClick={() => onChangePage(page)}
                     >{page}</span> )
             }
 
             { currentPage < maxPage-(showPageNumbers+1) ? <span title="Go to page..." onClick={() => goTo()}>...</span> : '' }
-            { currentPage < maxPage-showPageNumbers ? <span title="Go to last page" onClick={() => onChangePage(maxPage)}>{maxPage+1}</span> : '' }
+            { !pages.includes(maxPage) ? <span title="Go to last page" onClick={() => onChangePage(maxPage)}>{maxPage}</span> : '' }
             { currentPage < maxPage ? <span title="Go to next page" onClick={() => onChangePage(currentPage+1)}>&raquo;</span> : '' }
         </div> 
         : '' }
